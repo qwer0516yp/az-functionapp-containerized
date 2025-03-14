9c6e5bd7bb98ed4bd7896ed1fc7f25eca187cb4e
st_Akey <qwer0516yp@gmail.com>
2025-03-14 11:48:47 +0000

    add initial changes

---
+++ Src/Ping.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace Containerized.Function;
+
+public class Ping
+{
+    private readonly ILogger<Ping> _logger;
+
+    public Ping(ILogger<Ping> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function("Ping")]
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a Ping request at {Timestamp}", Datetime.Now);
+        return new OkObjectResult("Welcome to Azure Functions!");
+    }
+}
+
